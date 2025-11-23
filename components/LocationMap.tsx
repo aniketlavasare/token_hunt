8bfcfbe8f0b2dc1d170d54abcdf5833b508b6809
aavasare <aniketavasare2004@gmail.com>
2025-11-22 19:00:05 -0600

    mapbox integration

--- components/LocationMap.tsx
+++ components/LocationMap.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import { useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+import { useRef, useEffect, useState } from "react";
+import Map, { Marker, Popup, NavigationControl, GeolocateControl } from "react-map-gl/mapbox";
+import "mapbox-gl/dist/mapbox-gl.css";
 import { Card } from "@/components/ui/card";
-
-// Fix for default marker icon in react-leaflet
-const icon = L.icon({
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
 
 type LocationMapProps = {
   lat: number;
@@ -23,44 +11,75 @@ type LocationMapProps = {
   accuracy: number;
 };
 
-// Component to update map center when coordinates change
-function ChangeView({ center }: { center: [number, number] }) {
-  const map = useMap();
-  useEffect(() => {
-    map.setView(center, map.getZoom());
-  }, [center, map]);
-  return null;
-}
+const MAPBOX_TOKEN = "pk.eyJ1IjoiYW5pa2V0YXZhc2FyZTQyMCIsImEiOiJjbWlhbjRmcDMwejNqMmxvZ2x3bmZ1Y2duIn0.QAG-0y4eiR1quyf7XJoV4A";
 
 export default function LocationMap({ lat, lng, accuracy }: LocationMapProps) {
-  const position: [number, number] = [lat, lng];
+  const mapRef = useRef<any>(null);
+  const [showPopup, setShowPopup] = useState(true);
+
+  // Update map center when coordinates change
+  useEffect(() => {
+    if (mapRef.current) {
+      mapRef.current.flyTo({
+        center: [lng, lat],
+        duration: 1000,
+      });
+    }
+  }, [lat, lng]);
 
   return (
     <Card className="w-full overflow-hidden">
       <div className="relative h-[400px] w-full">
-        <MapContainer
-          center={position}
-          zoom={16}
-          style={{ height: "100%", width: "100%" }}
-          zoomControl={true}
+        <Map
+          ref={mapRef}
+          initialViewState={{
+            longitude: lng,
+            latitude: lat,
+            zoom: 16,
+          }}
+          style={{ width: "100%", height: "100%" }}
+          mapStyle="mapbox://styles/mapbox/streets-v12"
+          mapboxAccessToken={MAPBOX_TOKEN}
         >
-          <ChangeView center={position} />
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          <NavigationControl position="top-right" />
+          <GeolocateControl
+            position="top-right"
+            trackUserLocation
+            showUserHeading
           />
-          <Marker position={position} icon={icon}>
-            <Popup>
-              <div className="text-center">
-                <p className="font-semibold">You are here!</p>
-                <p className="text-xs text-gray-600">
-                  {lat.toFixed(6)}, {lng.toFixed(6)}
+          
+          <Marker
+            longitude={lng}
+            latitude={lat}
+            anchor="bottom"
+            onClick={() => setShowPopup(true)}
+          >
+            <div className="relative">
+              <div className="absolute -top-10 -left-4 h-8 w-8 bg-red-500 rounded-full border-4 border-white shadow-lg animate-pulse"></div>
+              <div className="absolute -top-10 -left-4 h-8 w-8 bg-red-500 rounded-full opacity-25 animate-ping"></div>
+            </div>
+          </Marker>
+
+          {showPopup && (
+            <Popup
+              longitude={lng}
+              latitude={lat}
+              anchor="top"
+              onClose={() => setShowPopup(false)}
+              closeOnClick={false}
+            >
+              <div className="text-center p-2">
+                <p className="font-semibold text-sm">📍 You are here!</p>
+                <p className="text-xs text-gray-600 mt-1">
+                  {lat.toFixed(7)}, {lng.toFixed(7)}
                 </p>
-                <p className="text-xs text-gray-500">Accuracy: ±{accuracy.toFixed(1)}m</p>
+                <p className="text-xs text-gray-500 mt-1">
+                  Accuracy: ±{accuracy.toFixed(1)}m
+                </p>
               </div>
             </Popup>
-          </Marker>
-        </MapContainer>
+          )}
+        </Map>
       </div>
     </Card>
   );
